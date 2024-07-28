eda2632e974ef61a7938f2639372fa6802ad6c3e
deepeshrohilla <deepeshrohilla211@gmail.com>
2024-07-28 20:44:15 +0530

    feat - services pae

---
+++ src/app/blinds/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+import WeDeliverBestQualitySection from "../components/WeDeliverBestQualitySection";
+import WeProvideSection2 from "../components/WeProvideSection2";
+import ServicePageBanner from "../components/ServicePageBanner";
+import curtain_banner_desk from "../assets/images/curtain_banner.png";
+import curtain_banner_mob from "../assets/images/curtain_banner_mob.png";
+import ContactUS from "../components/ContactUs";
+import ServicesFurnitureRange from "../components/ServicesFurnitureRange";
+
+const page = () => {
+  return (
+    <main className="service__page__wrapper blinds">
+      <ServicePageBanner
+        desktopImg={curtain_banner_desk}
+        mobileImg={curtain_banner_mob}
+        headText="Get your Home styled by Our Luxury Fabrics"
+        headSubText="Curtains"
+      />
+      <WeProvideSection2 />
+      <ServicesFurnitureRange />
+      <ContactUS hideContactInfo={true} />
+      <WeDeliverBestQualitySection
+        heading="We Deliver Best Product Quality"
+        paragraph={
+          <>
+            <p>
+              Welcome to Furniture Store, your go-to destination for quality and
+              stylish furniture. At Furniture Store, we believe in creating
+              spaces that reflect your personality and lifestyle. Whether you
+              are furnishing your home or office, we offer a wide range of
+              furniture options to suit every taste and budget.
+              <br />
+              Our team of skilled craftsmen ensures that each piece is crafted
+              with precision and attention to detail. We source the finest
+              materials to create furniture that not only looks great but is
+              also durable and functional.
+              <br />
+              Visit our showroom or explore our online catalog to discover a
+              curated selection of sofas, chairs, tables, and more. We are
+              committed to providing exceptional customer service and making
+              your furniture shopping experience enjoyable and stress-free.
+              <br />
+              Our team of skilled craftsmen ensures that each piece is crafted
+              with precision and attention to detail. We source the finest
+              materials to create furniture that not only looks great but is
+              also durable and functional.
+              <br />
+              Visit our showroom or explore our online catalog to discover a
+              curated selection of sofas, chairs, tables, and more. We are
+              committed to providing exceptional customer service and making
+              your furniture shopping experience enjoyable and stress-free.
+            </p>
+          </>
+        }
+      />
+    </main>
+  );
+};
+
+export default page;
--- src/app/components/ServicesFurnitureCards.tsx
+++ src/app/components/ServicesFurnitureCards.tsx
@@ -1,30 +1,42 @@
 import React from "react";
 import img from "../assets/images/fur_cur_01.png";
 import Image from "next/image";
+import { useAppDispatch } from "../lib/hooks";
+import { modalOpen } from "../lib/features/modalSlice";
 
 const ServicesFurnitureCards = ({ cards }: any) => {
+  const dispatch = useAppDispatch();
   return (
-    <div className="services__furniture__cards">
-      <div className="card">
-        <div className="img">
-          <Image src={img} alt="img" />
-        </div>
-        <div className="img_tag">
-          <span className="color_circle"></span>
-          <span className="tag">Floral Linen Sheer Curtain</span>
-        </div>
-        <div className="card_body">
-          <h5>
-            Sheer Curtains
-            <button type="button">Enquire Now</button>
-          </h5>
-          <p>
-            Be it end-to-end interiors, renovation or modular solutions, we have
-            it all for your home or office.
-          </p>
-        </div>
+    cards?.length && (
+      <div className="services__furniture__cards">
+        {cards?.map((item: any, key: number) => (
+          <div className="card" key={key}>
+            <div className="img">
+              <Image src={item?.img || img} alt="img" />
+            </div>
+            <div className="img_tag">
+              <span
+                className="color_circle"
+                style={{ backgroundColor: item?.tagColor }}
+              ></span>
+              <span className="tag">{item?.tag}</span>
+            </div>
+            <div className="card_body">
+              <h5>
+                {item?.cardTitle}
+                <button
+                  type="button"
+                  onClick={() => dispatch(modalOpen({ open: true }))}
+                >
+                  Enquire Now
+                </button>
+              </h5>
+              <p>{item?.cardPara}</p>
+            </div>
+          </div>
+        ))}
       </div>
-    </div>
+    )
   );
 };
 
